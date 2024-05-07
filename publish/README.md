c089f5c2f5b8b434151613263d5552ef6124ffec
Mykyta Biliaiev <thewalkingundeads@gmail.com>
2024-05-07 11:02:01 +0300

    new code verison

---
+++ publish/README.md
@@ -0,0 +1,1 @@
+API client that provide services of DataForSeo API. For more information about client code generation, please follow to github
---
+++ publish/dataforseo_client/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from dataforseo_client.api.app_data_api import AppDataApi
+from dataforseo_client.api.appendix_api import AppendixApi
+from dataforseo_client.api.backlinks_api import BacklinksApi
+from dataforseo_client.api.business_data_api import BusinessDataApi
+from dataforseo_client.api.content_analysis_api import ContentAnalysisApi
+from dataforseo_client.api.content_generation_api import ContentGenerationApi
+from dataforseo_client.api.dataforseo_labs_api import DataforseoLabsApi
+from dataforseo_client.api.domain_analytics_api import DomainAnalyticsApi
+from dataforseo_client.api.keywords_data_api import KeywordsDataApi
+from dataforseo_client.api.merchant_api import MerchantApi
+from dataforseo_client.api.on_page_api import OnPageApi
+from dataforseo_client.api.serp_api import SerpApi
+
