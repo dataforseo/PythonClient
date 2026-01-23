64546e6225a9950725b949433bb5fb30271c3a55
mykyta.biliaiev <mykyta.biliaev@softoria.com>
2026-01-23 21:36:33 +0200

    upd setup file

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 NAME = "dataforseo-client"
-VERSION = "2.0.17"
+VERSION = "2.0.18"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
-    "urllib3 >= 1.25.3, < 2.2.3",
+    "urllib3 >= 1.25.3",
     "python-dateutil",
     "pydantic >= 2",
     "typing-extensions >= 4.7.1",
