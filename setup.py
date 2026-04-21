b23026174627da30856ee889ffeef80e595a6f80
mykyta.biliaiev <mykyta.biliaev@softoria.com>
2026-04-21 09:26:05 +0300

    add publish workflow

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup, find_packages  
+import os
+from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 NAME = "dataforseo-client"
-VERSION = "2.0.23"
+VERSION = os.environ["PACKAGE_VERSION"]
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3",
